f5d76cb5dac0f233020196e6c1c4a8f9bec06f99
negaonka <neha.n.gaonkar@gmail.com>
2024-02-06 23:06:38 +0000

    Initial draft

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
